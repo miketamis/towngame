3f69a206be44c1e273a5d0725953b92928354921
Jareth MacArthur <jtmaca@gmail.com>
2014-05-18 18:57:51 +1200

    added level to enemy

--- src/main/java/com/TownGame/towngame/Enemy.java
+++ src/main/java/com/TownGame/towngame/Enemy.java
@@ -1,7 +1,10 @@
+package com.TownGame.towngame;
+
 public class Enemy extends Entity {
   public static int attack;
   public static int defense;
   public static int size;
+  public static int level;
 
 
 
